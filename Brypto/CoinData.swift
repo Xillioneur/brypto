e8a3179c3e18296fc85cd197abb39af2d688775c
SlickJohnson <liwa.johnson@gmail.com>
2019-03-26 23:32:44 -0700

    Add en_US price formatting

--- Brypto/CoinData.swift
+++ Brypto/CoinData.swift
@@ -66,4 +66,19 @@ class Coin {
       self.image = image
     }
   }
+
+  func priceAsString() -> String {
+    if price == 0.0 {
+      return "Loading..."
+    }
+
+    let formatter = NumberFormatter()
+    formatter.locale = Locale(identifier: "en_US")
+    formatter.numberStyle = .currency
+    if let fancyPrice = formatter.string(from: NSNumber(floatLiteral: price)) {
+      return fancyPrice
+    } else {
+      return "ERROR"
+    }
+  }
 }
--- Brypto/CryptoTableViewController.swift
+++ Brypto/CryptoTableViewController.swift
@@ -28,13 +28,12 @@ class CryptoTableViewController: UITableViewController, CoinDataDelegate {
     return CoinData.shared.coins.count
   }
 
-
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = UITableViewCell()
 
     let coin = CoinData.shared.coins[indexPath.row]
 
-    cell.textLabel?.text = "\(coin.symbol) - \(coin.price)"
+    cell.textLabel?.text = "\(coin.symbol) - \(coin.priceAsString())"
     cell.imageView?.image = coin.image
 
 
